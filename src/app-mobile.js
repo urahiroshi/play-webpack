46800307df1bdaacff5e0afe5ff65ebc3bac8e83
urahiroshi <urahiroshi@mercari.com>
2018-09-21 11:02:04 +0900

    Add sample code from https://medium.com/dailyjs/webpack-4-splitchunks-plugin-d9fbbe091fd0

---
+++ src/app-mobile.js
@@ -0,0 +1,10 @@
+// This code is from https://medium.com/dailyjs/webpack-4-splitchunks-plugin-d9fbbe091fd0
+
+import("react");
+import("lodash");
+import "jquery";
+
+console.log('I am mobile');
+var b = 10;
+
+export default b;
