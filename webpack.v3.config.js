b99995b9c0da0f59234f73faf382dbd43e9e0b04
urahiroshi <urahiroshi@mercari.com>
2018-09-21 10:55:07 +0900

    Initial commit

---
+++ webpack.v3.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    'app-mobile': path.resolve(__dirname, './src/app-mobile.js'),
+    'app-desktop': path.resolve(__dirname, './src/app-desktop.js')
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'lib',
+      chunks: ['app-mobile', 'app-desktop'],
+      minChunks: module => {
+        return module.context && module.context.indexOf('node_modules') !== -1;
+      }
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: '[name].js'
+  },
+  target: 'web'
+};
